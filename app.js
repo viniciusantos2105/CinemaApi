ed33a84c1721b55b525875e58fa7a8708bf2d1e4
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-09 16:17:44 -0300

    Configurando

--- app.js
+++ app.js
@@ -5,7 +5,12 @@
     const mongoose = require("mongoose")
     const app = express()
 //Configurações
-
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    //Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
 //Rotas
 
 //Outros
