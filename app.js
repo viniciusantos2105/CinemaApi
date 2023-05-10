f3f535417c35c2b8d0e00438f36ea76919c30754
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-10 15:49:04 -0300

    Implementando registro de filmes e config mongoose

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
     const bodyParser = require("body-parser")
     const app = express()
     const catalog = require("./routes/catalog")
+    const admin = require("./routes/admin")
+    const mongoose = require("mongoose")
 //Configurações
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
@@ -13,12 +15,20 @@
         app.engine('handlebars', handle.engine);
         app.set('view engine', 'handlebars');
     //Mongoose
-
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://127.0.0.1:27017/cinemaapi?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.1", {   
+            useNewUrlParser:true,
+            useUnifiedTopology: true
+        }).then(() => {
+            console.log("MongoDB Conectado...")
+        }).catch((err) => {
+            console.log("Houve um erro ao se conectar ao mongoDB: "+ err)
+        })
 //Rotas
     app.get('/', (req, res) =>{
         res.send("Rota principal")
     })
-
+    app.use('/admin', admin)
     app.use('/catalog', catalog)
 //Outros
 const PORT = 8081
---
+++ routes/admin.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const router = express.Router()
+const mongoose = require("mongoose")
+require("../models/Movie")
+const Movie = mongoose.model("movie")
+
+router.get('/register', (req, res) =>{
+    res.render("admin/register")
+})
+
+router.post("/register/movie", (req, res) =>{
+
+    var erros = []
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if(!req.body.gender || typeof req.body.gender == undefined || req.body.gender == null){
+        erros.push({texto: "Gênero inválido"})
+    }
+
+    if(!req.body.wallpaper || typeof req.body.wallpaper == undefined || req.body.wallpaper == null){
+        erros.push({texto: "Link inválido"})
+    }
+
+    if(req.body.name.length < 2){
+        erros.push({texto: "Nome do filme é muito pequeno"})
+    }
+
+    if(req.body.wallpaper.length < 10){
+        erros.push({texto: "Link do wallpaper filme é inválido"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/register", {erros: erros})
+    }
+    else{
+        const newMovie = {
+            name: req.body.name,
+            classification: req.body.classification,
+            gender: req.body.gender,
+            wallpaper: req.body.wallpaper
+        }
+
+        new Movie(newMovie).save().then(() =>{
+            console.log("Filme salvo com sucesso!")
+            res.redirect("/")
+        }).catch((err) =>{
+            console.log("Erro ao salvar filme")
+        })
+    }
+})
+
+module.exports = router
