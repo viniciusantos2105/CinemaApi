c2f981a736f95526bd8335191168f72df64948a8
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-16 14:32:00 -0300

    adicionando pag de catalogo

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
+require("../models/Movie")
+const Movie = mongoose.model("movie")
 
 router.get('/', (req, res) =>{
-    res.render("catalog/index")
+    Movie.find().lean().sort({date: "desc"}).then((movies) =>{    
+        res.render("catalog/index", {movies: movies})
+    })
 })
 
 module.exports = router
