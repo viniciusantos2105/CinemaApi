4b8e1bda254933a22b344d33dee2dfb053c5c901
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-22 14:50:34 -0300

    finalizando implementação de compra de ingresso

--- routes/user.js
+++ routes/user.js
@@ -7,27 +7,18 @@ require("../models/MovieTheater")
 const MovieTheater = mongoose.model("movieTheater")
 require("../models/Session")
 const Session = mongoose.model("session")
+require("../models/Ticket")
+const Ticket = mongoose.model("ticket")
+
 
 router.get("/buy/ticket/:id", (req, res)=>{
     Session.findById({_id: req.params.id}).lean().then((session)=>{
         const str = session.movie
         Movie.findById({_id: str.toString()}).lean().then((movie)=>{
-            const ticket = ({
-                price: req.body.price,
-                type: req.body.type,
-                movie: req.body.movie,
-                movieTheater: req.body.movieTheater,
-                hour: req.body.hour,
-                quantity: req.body.quantity
+            const str = session.movieTheater
+            MovieTheater.findById({_id: str.toString()}).lean().then((theater)=>{
+                res.render("user/buyTicket", {session: session, movie: movie, theater: theater})
             })
-            if(req.body.type == "INTEIRA"){
-
-                ticket.price = 35
-                res.render("user/buyTicket", {session: session, movie: movie, ticket: ticket})
-            }else{
-                ticket.price = 17.50
-                res.render("user/buyTicket", {session: session, movie: movie, ticket: ticket})
-            }
         })
         .catch((err)=>{
             console.log(err)
@@ -37,5 +28,44 @@ router.get("/buy/ticket/:id", (req, res)=>{
     })
 })
 
+router.post("/buy/ticket", (req, res)=>{
+
+    const newTicket = ({
+        type: req.body.type,
+        movie: req.body.movie,
+        movieTheater: req.body.theater,
+        hour: req.body.hour,
+        quantity: req.body.quantity,
+        price: 0
+    })
+    if(newTicket.type == 'INTEIRA' && newTicket.quantity > 1){
+        newTicket.price = 32.00 * newTicket.quantity
+        new Ticket(newTicket).save().then(()=>{
+            console.log("Ticket comprado com sucesso")
+            res.render("user/buySuccess")
+        })
+    }
+    else if(newTicket.type == 'INTEIRA' && newTicket.quantity == 1){
+        newTicket.price = 32.00 
+        new Ticket(newTicket).save().then(()=>{
+            console.log("Ticket comprado com sucesso")
+            res.render("user/buySuccess")
+        })
+    }
+    else if(newTicket.type == 'MEIA' && newTicket.quantity > 1){
+        newTicket.price = 16.00 * newTicket.quantity
+        new Ticket(newTicket).save().then(()=>{
+            console.log("Ticket comprado com sucesso")
+            res.render("user/buySuccess")
+        })
+    }
+    else if(newTicket.type == 'MEIA' && newTicket.quantity == 1){
+        newTicket.price = 16.00
+        new Ticket(newTicket).save().then(()=>{
+            console.log("Ticket comprado com sucesso")
+            res.render("user/buySuccess")
+        })
+    }
+})
 
 module.exports = router
